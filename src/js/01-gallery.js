043a1858a5353a6e5c1e900a4e21acc2c0793ac1
Perslay <natalia.44.fedyk@gmail.com>
2024-02-01 13:13:11 +0100

    Task 1 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import SimpleLightbox from 'simplelightbox';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+
+const list = document.querySelector('.gallery');
+
+const listItems = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li><a class="gallery__item" href=${original}>
+      <img class="gallery__image" src=${preview} alt="${description}" />
+      </a></li>`
+  )
+  .join('');
+
+list.insertAdjacentHTML('afterbegin', listItems);
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  captionPosition: 'bottom',
+});
 
 console.log(galleryItems);
