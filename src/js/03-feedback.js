f4d0d74c9ae3f33320ce92a35fe0defac4f14508
Perslay <natalia.44.fedyk@gmail.com>
2024-02-02 20:19:06 +0100

    Task 3 almost done
    Input values are saved every second in local storage if changed.
    The values in inputs come from local storage after refreshing the page if not empty.
    After submitting the form:
    - data from inputs is logged in console as object
    - inputs become empty
    - local storage key's value regarding input values becomes empty
    
    ISSUE: If only one input is filled, the value will not be saved after refreshing the page.

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,52 @@
+import throttle from 'lodash.throttle';
 
+const emailInput = document.querySelector("[name='email']");
+const messageInput = document.querySelector("[name='message']");
+const form = document.querySelector('.feedback-form');
+
+function saveInput() {
+  const email = emailInput.value;
+  const message = messageInput.value;
+  const data = {
+    email,
+    message,
+  };
+
+  try {
+    const stringedData = JSON.stringify(data);
+    localStorage.setItem('feedback-form-state', stringedData);
+  } catch (error) {
+    console.log('stringedData error: ' + error);
+  }
+}
+
+const throttleSave = throttle(saveInput, 500);
+
+emailInput.addEventListener('input', throttleSave);
+messageInput.addEventListener('input', throttleSave);
+form.addEventListener('submit', onSubmit);
+
+function onSubmit(event) {
+  event.preventDefault();
+  const sentData = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  console.log(sentData);
+  localStorage.removeItem('feedback-form-state');
+  emailInput.value = null;
+  messageInput.value = null;
+}
+
+try {
+  const parsedData = JSON.parse(localStorage.getItem('feedback-form-state'));
+  if (parsedData.email !== '' && parsedData.message !== '') {
+    // console.log('Local storage is NOT empty');
+    emailInput.value = parsedData.email;
+    messageInput.value = parsedData.message;
+  } else {
+    // console.log('Local storage is empty');
+  }
+} catch (error) {
+  console.log('parsedData error: ' + error);
+}
